5a4f26cf7f5414f35338a6b8813d00c68fcb2745
DannielN <novacekd.05@spst.eu>
2022-03-03 11:04:15 +0100

    S metodama

--- Pr01/Pr01/Form1.cs
+++ Pr01/Pr01/Form1.cs
@@ -29,7 +29,6 @@ namespace Pr01
             int poziceMax = 0;
             int poziceMin = 0;
             double soucet = 0;
-            int pocet = 0;
             for (int i = 0; i < n; i++)
             {
                 pole[i] = rnd.NextDouble() * (999) + 1;
@@ -58,6 +57,42 @@ namespace Pr01
             {
                 listBox2.Items.Add(pole[i].ToString("F2"));
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            int n = Convert.ToInt32(textBox1.Text);
+            double[] pole = new double[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                pole[i] = rnd.NextDouble() * (999) + 1;
+                listBox1.Items.Add(pole[i].ToString("F2"));
+            }
+
+            double max = pole.Max();
+            double min = pole.Min();
+            double soucet = pole.Sum();
+            soucet = (soucet - max) - (min);
+            double prumer = soucet / (pole.Length - 2);
+
+            int PoziceMax = Array.IndexOf(pole, max);
+            int PoziceMin = Array.IndexOf(pole, min);
+            pole[PoziceMax] = min;
+            pole[PoziceMin] = max;
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                listBox2.Items.Add(pole[i].ToString("F2"));
+            }
+
+            label1.Text = ("Maximum je = " + max.ToString("F2"));
+            label2.Text = ("Minimum je = " + min.ToString("F2"));
+            label3.Text = ("Aritmetický průměr bez maxima a minima je = " + prumer.ToString("F2"));
         }
     }
 }
