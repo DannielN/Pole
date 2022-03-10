d48c05f3ef6b4b332723d4aed57927a9af57f1b7
DannielN <novacekd.05@spst.eu>
2022-03-10 10:42:56 +0100

    Pr03 hotovo

--- Pr01/Pr03/Form1.cs
+++ Pr01/Pr03/Form1.cs
@@ -19,7 +19,20 @@ namespace Pr03
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) /* pole A o N nahodnych cislech*/
+        {}
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+
+        private void button2_Click(object sender, EventArgs e)
+        {}
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
             int N = Convert.ToInt32(textBox1.Text);
@@ -34,12 +47,7 @@ namespace Pr03
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)
         {
             listBox2.Items.Clear();
             int M = Convert.ToInt32(textBox2.Text);
@@ -54,11 +62,37 @@ namespace Pr03
             }
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
             listBox3.Items.Clear();
+            int[] poleC = poleA.Concat(poleB).ToArray();
 
+            for (int k = 0; k < poleC.Length; k++)
+            {
+                listBox3.Items.Add(poleC[k]);
+            }
+        }
 
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            listBox4.Items.Clear();
+            int[] poleD = poleA.Union(poleB).ToArray();
+
+            for (int k = 0; k < poleD.Length; k++)
+            {
+                listBox4.Items.Add(poleD[k]);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox5.Items.Clear();
+            int[] poleE = poleA.Intersect(poleB).ToArray();
+
+            for (int k = 0; k < poleE.Length; k++)
+            {
+                listBox5.Items.Add(poleE[k]);
+            }
         }
     }
 }
